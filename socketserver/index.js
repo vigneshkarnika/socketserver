f3e3e5b1ac02ab4284ecee3ad8b0d57413fdcffc
vignesh <vignesh@admins-MacBook-Pro.local>
2021-03-27 19:59:52 +0530

    init

---
+++ socketserver/index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+const httpServer = require("http").createServer(app);
+const options = {};
+const io = require("socket.io")(httpServer, options);
+
+const redisAdapter = require("socket.io-redis");
+io.adapter(redisAdapter({ host: process.env.REDIS_URL, port: 6379 }));
+app.use(express.static("public"));
+app.get("/publish", function (req, res) {
+  try {
+    console.log(req.query.app, req.query.topic, req.query.data);
+    io.sockets.emit(req.query.app + "/" + req.query.topic, req.query.data);
+    io.sockets.emit(
+      "appdebug",
+      `${req.query.app}/${req.query.topic}: ${req.query.data}`
+    );
+    res.send({
+      status: "success",
+    });
+  } catch (error) {
+    res.send({
+      status: "failed",
+    });
+  }
+});
+
+app.post("/publish", function (req, res) {
+  try {
+    console.log(req.body);
+    console.log(req.body.app, req.body.topic, req.body.data);
+    io.sockets.emit(
+      "appdebug",
+      `${req.body.app}/${req.body.topic}: ${JSON.stringify(req.body.data)}`
+    );
+    res.send({
+      status: "success",
+    });
+  } catch (error) {
+    res.send({
+      status: "failed",
+    });
+  }
+});
+app.get("/log", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+io.on("connection", (socket) => {
+  console.log("new client connected");
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+httpServer.listen(3004, function () {
+  console.log(`Server running on 3004 port, PID: ${process.pid}`);
+});
